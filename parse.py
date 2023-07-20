16289b433b084d9d78303a3c93d1fde752fa6a50
v1605 <55302877+v1605@users.noreply.github.com>
2023-07-20 17:00:42 -0400

    Create parse.py

---
+++ parse.py
@@ -0,0 +1,16 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+import csv
+
+with open('necStandardDisneyCodes.csv', newline='') as csvfile:
+
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    
+    for row in spamreader:
+        print('\t- platform: template')
+        print('\t\tname: "' + row[0] + '"')
+        print('\t\ton_press:')
+        print('\t\t\t- remote_transmitter.transmit_nec:')
+        print('\t\t\t\taddress: ' + row[1])
+        print('\t\t\t\taddress: ' + row[2])
